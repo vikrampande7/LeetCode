467f723d997f11f69fb32887722855ad0e56f105
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2024-07-17 18:13:30 -0400

    Time: 54 ms (85.03%), Space: 17.7 MB (25.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        '''Find difference of target and values in array, add them to hashmap'''
         hashMap = {}
         for i, num in enumerate(nums):
             difference = target - num
@@ -9,25 +8,4 @@ class Solution:
                 return [hashMap[difference], i]
             
             hashMap[num] = i
-            
-            
-          
-        # BruteForce
-        #   for i in range(0, len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] + nums[j] == target:
-        #             return i, j
-        
-        
-#         num_indices = {}
-#         for i, num in enumerate(nums):
-#             complement = target - num
-            
-#             if complement in num_indices:
-#                 return [num_indices[complement], i]
-
-#             num_indices[num] = i
-        
-                
-        
         
