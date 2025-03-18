15bce5ab5bb1f32a20bcbaddab008f5c2996caab
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-03-18 17:41:02 -0400

    Time: 541 ms (73.63%), Space: 20.6 MB (84.6%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        """
-            If we use bruteforce with three loops, we would encounter problem with duplicates and time complexity would be -> O(n**3)
-            Solution:
-                - Sort the given array so as to avoid duplicates, if neighbor duplicate is found, skip that.
-                - Get the first element and then solve the problem with TwoSum approach
-                - Set up the pointers
-                - Set up the conditions for sum <> 0 and update pointers accordingly
-                - Look for the edgecases
-                - Time Complexity -> o(n**2)
-        """
-        
-        out = []
-        nums.sort()
-        
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
+        res = []
+        nums.sort() # O(n^2)
+
+        for i, v in enumerate(nums):
+            if i > 0 and v == nums[i-1]:
                 continue
-                
-            l, r = i+1, len(nums)-1
-            while l<r:
-                tS = a + nums[l] + nums[r]
-                if tS > 0:
+        
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                ts = v + nums[l] + nums[r]
+
+                if ts > 0:
                     r -= 1
-                elif tS < 0:
+                elif ts < 0:
                     l += 1
                 else:
-                    out.append([a, nums[l], nums[r]])
+                    res.append([v, nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l-1] and l<r:
+
+                    while nums[l] == nums[l-1] and l < r:
                         l += 1
-                        
-        return out
+            
+        return res
+        
