35fe9e9a08eee8c689a6849cbde1aa4823ddd8cd
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-03-22 12:48:41 -0400

    Time: 119 ms (7.49%), Space: 28.4 MB (67.5%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,34 +1,22 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        '''
-        Bruteforce Solution: Using two loops but time complexity is o(n**2)
-        area = 0 
-        for i in range(len(height)):
-            for j in range(i+1, len(height)):
-                a = min(height[i],height[j]) * (i-j) # breadth * minimum(length)
-                area = max(a, area)
-        return area
-        
-        But to do this in o(n)
-        - We just check the broader breadth
-        - For that if left pointer is less than right pointer we shift out left pointer to right 
-        - and vice versa
-        - so that we would maximize the area
-        '''
-        area = 0
+
+        maxArea = 0
         l = 0
         r = len(height) - 1
-        
+
         while l < r:
-            a = min(height[l],height[r]) * (r-l) # breadth * minimum(length)
-            area = max(a, area)
-            
+            area = (r-l) * min(height[l], height[r])
+            maxArea = max(area, maxArea)
+
             if height[l] < height[r]:
                 l += 1
-            elif height[l] < height[r]:
+            elif height[r] > height[l]:
                 r -= 1
             else:
                 r -= 1
-                
-        return area
+
+        return maxArea
+
+
         
