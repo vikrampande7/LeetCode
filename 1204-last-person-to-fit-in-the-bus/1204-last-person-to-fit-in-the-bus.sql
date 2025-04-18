84dc5f8b932a43ad616392561043845360f9e2b1
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-04-18 19:33:29 -0400

    Time: 1680 ms (40.65%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,30 +1,45 @@
 # Write your MySQL query statement below
-WITH weight_sums AS (
-    SELECT 
-        person_id,
-        person_name,
-        weight,
-        turn,
-        SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
-    FROM Queue 
-)
+-- WITH weight_sums AS (
+--     SELECT 
+--         person_id,
+--         person_name,
+--         weight,
+--         turn,
+--         SUM(weight) OVER (ORDER BY turn) AS cumulative_weight
+--     FROM Queue 
+-- )
 
-SELECT
-    person_name
-FROM
-(
+-- SELECT
+--     person_name
+-- FROM
+-- (
+-- SELECT 
+--     person_id,
+--     person_name,
+--     weight,
+--     turn,
+--     COALESCE(
+--         LAG(cumulative_weight) OVER (ORDER BY turn),
+--         weight
+--     ) AS sum_previous_weights
+-- FROM weight_sums
+-- ) AS tmp
+-- WHERE sum_previous_weights < 1000
+-- ORDER BY sum_previous_weights DESC
+-- LIMIT 1
+
 SELECT 
-    person_id,
-    person_name,
-    weight,
-    turn,
-    COALESCE(
-        LAG(cumulative_weight) OVER (ORDER BY turn),
-        weight
-    ) AS sum_previous_weights
-FROM weight_sums
-) AS tmp
-WHERE sum_previous_weights < 1000
-ORDER BY sum_previous_weights DESC
+    a.person_name
+FROM
+    Queue a
+JOIN
+    Queue b
+ON 
+    a.turn >= b.turn
+GROUP BY
+    a.turn
+HAVING 
+    SUM(b.weight) <= 1000
+ORDER BY 
+    SUM(b.weight) DESC
 LIMIT 1
-
