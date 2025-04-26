5f669b5f2a227fcb0b21b000d16a448561bb5845
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-04-26 08:58:19 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    CASE WHEN MOD(id,2) = 0 THEN id - 1
+         WHEN MOD(id,2) = 1 AND id + 1 IN (SELECT id FROM seat) THEN id + 1
+         ELSE id
+    END AS id,
+    student
+FROM
+    Seat
+
