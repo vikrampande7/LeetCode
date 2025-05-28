7be2b0d7fef26001c183f374e4530c972acef9c5
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-05-28 17:14:58 -0400

    Time: 147 ms (23.43%), Space: 35.6 MB (45.49%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,34 +1,57 @@
 from itertools import combinations, permutations
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
+        hashmap = {}
         longest = 0
-        res = []
 
-        def check_palindrome(s):
-            i, j = 0, len(s)-1
-            is_pali = True
-            while i < j:
-                if s[i] != s[j]:
-                    is_pali = False
-                    break
-                i += 1
-                j -= 1
+        for word in words:
+            r_word = word[::-1]
+            if hashmap.get(r_word, 0) > 0:
+                longest += 4
+                hashmap[r_word] -= 1
+            else:
+                hashmap[word] = hashmap.get(word, 0) + 1
 
-            if is_pali: 
-                return len(s) 
-            else: 
-                return 0
-
-        for i in range(1, len(words)+1):
-            for combo in permutations(words, i):
-                res.append(''.join(combo))
-
-        for combo in res:
-            #print(combo)
-            l = check_palindrome(combo)
-            longest = max(longest, l)
+        for word, count in hashmap.items():
+            if word[0] == word[1] and count > 0:
+                longest += 2
+                break
 
         return longest
+
+
+
+
+
+        # Memory Exceeded
+        # longest = 0
+        # res = []
+
+        # def check_palindrome(s):
+        #     i, j = 0, len(s)-1
+        #     is_pali = True
+        #     while i < j:
+        #         if s[i] != s[j]:
+        #             is_pali = False
+        #             break
+        #         i += 1
+        #         j -= 1
+
+        #     if is_pali: 
+        #         return len(s) 
+        #     else: 
+        #         return 0
+
+        # for i in range(1, len(words)+1):
+        #     for combo in permutations(words, i):
+        #         res.append(''.join(combo))
+
+        # for combo in res:
+        #     #print(combo)
+        #     l = check_palindrome(combo)
+        #     longest = max(longest, l)
+
+        # return longest
 
 
 
