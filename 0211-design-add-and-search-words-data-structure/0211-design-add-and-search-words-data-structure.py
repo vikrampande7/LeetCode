0faf4b197da4dd40d8b38e06157a6e3193678a64
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-07-01 21:29:13 -0400

    Time: 1049 ms (88.08%), Space: 65.8 MB (34.75%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,46 +1,36 @@
-"""
-Recursion when string contains DOT
-"""
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = False
+        self.eow = False
 
 class WordDictionary:
 
     def __init__(self):
         self.root = TrieNode()
-        
 
     def addWord(self, word: str) -> None:
-        crawler = self.root
+        curr = self.root
         for c in word:
-            if c not in crawler.children:
-                crawler.children[c] = TrieNode()
-            crawler = crawler.children[c]
-        crawler.word = True
-
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.eow = True
+        
     def search(self, word: str) -> bool:
-        def dfs(idx, root):
-            crawler = root
-            for i in range(idx, len(word)):
-                c = word[i]
-                if c == ".":
-                    for child in crawler.children.values():
-                        if dfs(i+1, child):
-                            return True
+        def dfs(node, i):
+            if i == len(word):
+                return node.eow
+            
+            c = word[i]
+            if c == ".":
+                # Try all possible children
+                for child in node.children.values():
+                    if dfs(child, i + 1):
+                        return True
+                return False
+            else:
+                if c not in node.children:
                     return False
-                else:
-                    if c not in crawler.children:
-                        return False
-                    crawler = crawler.children[c]
-            return crawler.word
+                return dfs(node.children[c], i + 1)
         
-        return dfs(0, self.root)
-        
-
-
-# Your WordDictionary object will be instantiated and called as such:
-# obj = WordDictionary()
-# obj.addWord(word)
-# param_2 = obj.search(word)
+        return dfs(self.root, 0)
