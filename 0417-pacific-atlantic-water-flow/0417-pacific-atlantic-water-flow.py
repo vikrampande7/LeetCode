f0068e77b205ce59be54ee5dee6a6b94fee2d41b
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-07-07 09:46:33 -0400

    Time: 39 ms (41.27%), Space: 19.4 MB (65.14%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,36 +1,31 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
-        
-        def dfs(r, c, ocean, prevH):
-            if (
-                (r, c) in ocean or
-                r < 0 or
-                c < 0 or
-                r == ROWS or
-                c == COLS or
-                heights[r][c] < prevH    
-            ):
-                return 
-            
-            ocean.add((r, c))
-            dfs(r+1, c, ocean, heights[r][c])
-            dfs(r-1, c, ocean, heights[r][c])
-            dfs(r, c+1, ocean, heights[r][c])
-            dfs(r, c-1, ocean, heights[r][c])
-        
+        ROWS = len(heights)
+        COLS = len(heights[0])
+        atl, pac = set(), set()
+        out = []
+
+        def dfs(r, c, visit, ph):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS or heights[r][c] < ph or (r, c) in visit):
+                return
+
+            visit.add((r, c))
+            dfs(r+1, c, visit, heights[r][c])
+            dfs(r-1, c, visit, heights[r][c])
+            dfs(r, c+1, visit, heights[r][c])
+            dfs(r, c-1, visit, heights[r][c])
+
         for c in range(COLS):
-            dfs(0, c, pac, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+            dfs(0, c, pac, heights[0][c]) # First row, all columns
+            dfs(ROWS-1, c, atl, heights[ROWS-1][c]) # Last row, all columns
 
         for r in range(ROWS):
-            dfs(r, 0, pac, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            dfs(r, 0, pac, heights[r][0]) # First col, all rows
+            dfs(r, COLS-1, atl, heights[r][COLS-1]) # Last col, all rows
 
-        res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
-        return res
+                if (r, c) in atl and (r, c) in pac:
+                    out.append([r, c])
+
+        return out
