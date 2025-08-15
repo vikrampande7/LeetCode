413483332cd20753792f0cf502a6cb6c9f923f1e
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-08-15 17:27:09 -0400

    Time: 3 ms (14.72%), Space: 18 MB (10.7%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,13 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        """
-        - Convert the Number to Binary
-        - Count 1s in the number
-        """
-        n = str(bin(n))
-        hamming_weight = 0
-        for i in n:
-            if i == "1":
-                hamming_weight+=1
-        return hamming_weight
+        cnt = 0
+        while n > 0:
+            n = n & (n-1)
+            cnt += 1
+        return cnt
         
