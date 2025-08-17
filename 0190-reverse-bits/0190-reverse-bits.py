5d014c7faa9bd6f49720b50acac4767781478910
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-08-17 19:39:59 -0400

    Time: 46 ms (14.94%), Space: 17.8 MB (30.25%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,3 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        return int(f'{n:032b}'[::-1],2)
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << (31 - i))
+        return res
