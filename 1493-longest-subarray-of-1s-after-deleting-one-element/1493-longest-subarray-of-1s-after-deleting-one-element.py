caddb4b71c8c6bc664a3699adfc678132a84e708
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-08-24 10:03:03 -0400

    Time: 64 ms (54.73%), Space: 21.7 MB (68.25%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,32 +1,44 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        n = len(nums)
+        ################# SLIDING WINDOW
         res = 0
-        ones = 0
-        for num in nums:
-            if num == 1:
-                ones += 1
+        i, j = 0, 0
+        last_zero_seen = -1
+        while j < len(nums):
+            if nums[j] == 0:
+                i = last_zero_seen + 1
+                last_zero_seen = j
+            res = max(res, j - i)
+            j += 1
+        return res
 
-        def findMax(nums, idx):
-            currLen = 0
-            maxLen = 0
-            for i in range(n):
-                if i == idx:
-                    continue
-                if nums[i] == 1:
-                    currLen += 1
-                    maxLen = max(maxLen, currLen)
-                else:
-                    currLen = 0
-            return maxLen
+        ################# BRUTE FORCE
+        # n = len(nums)
+        # res = 0
+        # ones = 0
+        # for num in nums:
+        #     if num == 1:
+        #         ones += 1
 
-        if ones == n:
-            return n - 1
-        elif ones == 0:
-            return 0
-        else:
-            for i in range(n):
-                if nums[i] == 0:
-                    res = max(res, findMax(nums, i))
-            return res
-        
+        # def findMax(nums, idx):
+        #     currLen = 0
+        #     maxLen = 0
+        #     for i in range(n):
+        #         if i == idx:
+        #             continue
+        #         if nums[i] == 1:
+        #             currLen += 1
+        #             maxLen = max(maxLen, currLen)
+        #         else:
+        #             currLen = 0
+        #     return maxLen
+
+        # if ones == n:
+        #     return n - 1
+        # elif ones == 0:
+        #     return 0
+        # else:
+        #     for i in range(n):
+        #         if nums[i] == 0:
+        #             res = max(res, findMax(nums, i))
+        #     return res     
