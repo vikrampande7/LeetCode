f4393a156c74c5b9d9dd3a5ba5c9ba51b42194a8
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-02 16:48:32 -0400

    Time: 7 ms (43.61%), Space: 25.7 MB (46.58%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,10 +3,29 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
-        for _ in range(k):
-            last = nums[-1]
-            for i in range(len(nums)):
-                nums[i], last = last, nums[i]
-        
-        
+        ### Brute Force - O(nk), O(1)
+        # k = k % len(nums)
+        # for _ in range(k):
+        #     last = nums[-1]
+        #     for i in range(len(nums)):
+        #         nums[i], last = last, nums[i]      
+
+        ### With Extra Space O(n), O(n)
+        # n = len(nums)
+        # a = [0] * n
+        # for i in range(n):
+        #     a[(i+k) % n] = nums[i]
+        # nums[:] = a
+
+        ### With Reverse O(1) SC and O(n) TC
+        def helper(nums, s, e):
+            while s < e:
+                nums[s], nums[e] = nums[e], nums[s]
+                s, e = s + 1, e - 1
+
+        n = len(nums)
+        k = k % n
+
+        helper(nums, 0, n-1)
+        helper(nums, 0, k-1)
+        helper(nums, k, n-1)
