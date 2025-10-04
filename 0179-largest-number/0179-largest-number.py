33466fdb370c72afabeabd5b1e633729e58df946
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-04 11:51:54 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        if len(nums) == 0:
+            return ""
+        perms = self.largestNumber(nums[1:])
+        res = []
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                res.append(p_copy)
+        return str(max(res))
+        
