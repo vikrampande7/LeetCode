677d698872cfbc10d7a0bdbcf5bc8d4e6e3e2aa7
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-05 13:40:11 -0400

    Time: 0 ms (100%), Space: 19 MB (78.64%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,15 +6,8 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        """
-        - DFS Recursive
-            - Check edge of when tree is null
-            - Otherwise if 1 root, sum is one
-            - Check for the children (left and right), add maximum length of connecting nodes
-        - DFS Iterative
-        - BFS Iterative
-        """
-        if root is None:
+        ### RECURSIVE
+        if not root:
             return 0
-        
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
