1bfcd0fead6c72f070419cc8806c042af39109ce
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-05 13:54:07 -0400

    Time: 0 ms (100%), Space: 19 MB (78.64%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -11,18 +11,32 @@ class Solution:
         #     return 0
         # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-        ### BFS with Queue
+        ### DFS with Stack - Iterative - PreOrder
         if not root:
             return 0
-        q = deque([root])
-        level = 0
-        while q:
-            for i in range(len(q)):
-                currNode = q.popleft()
-                if currNode.left:
-                    q.append(currNode.left)
-                if currNode.right:
-                    q.append(currNode.right)
-            level += 1
-        return level
+        stack = [[root, 1]]
+        res = 1
+        while stack:
+            currNode, depth = stack.pop()
+            res = max(res, depth)
+            if currNode.left:
+                stack.append([currNode.left, depth+1])
+            if currNode.right:
+                stack.append([currNode.right, depth+1])
+        return res
+
+        ### BFS with Queue - Iterative
+        # if not root:
+        #     return 0
+        # q = deque([root])
+        # level = 0
+        # while q:
+        #     for i in range(len(q)):
+        #         currNode = q.popleft()
+        #         if currNode.left:
+        #             q.append(currNode.left)
+        #         if currNode.right:
+        #             q.append(currNode.right)
+        #     level += 1
+        # return level
         
