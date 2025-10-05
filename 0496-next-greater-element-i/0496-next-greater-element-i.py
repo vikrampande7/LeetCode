ff9090f40bdf59da6dda77d7b9f8e9edb5759dd8
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-05 14:02:49 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,29 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # res = [-1] * len(nums1)
+        # for i, num1 in enumerate(nums1):
+        #     found = False
+        #     for j, num2 in enumerate(nums2):
+        #         if found and num2 > num1:
+        #             res[i] = num2
+        #             break
+        #         if num2 == num1:
+        #             found = True
+        # return res
+
+        ## Monotonous Stack
+        stack = []
+        hashmap = {}
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hashmap[stack.pop()] = num
+            stack.append(num)
         res = [-1] * len(nums1)
-        for i, num1 in enumerate(nums1):
-            found = False
-            for j, num2 in enumerate(nums2):
-                if found and num2 > num1:
-                    res[i] = num2
-                    break
-                if num2 == num1:
-                    found = True
+        for i in range(len(nums1)):
+            if nums1[i] in hashmap:
+                res[i] = hashmap[nums[i]]
         return res
+
 
 
         
