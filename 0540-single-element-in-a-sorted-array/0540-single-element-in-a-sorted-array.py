dccefcc70f1182835904da74a630fd3c1d2a934e
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-08 19:01:21 -0400

    Time: 0 ms (100%), Space: 24.9 MB (16.65%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,8 +1,22 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        hashmap = {}
-        for n in nums:
-            hashmap[n] = hashmap.get(n, 0) + 1
-        for k, v in hashmap.items():
-            if v == 1:
-                return k
+        #### O(n), O(n)
+        # hashmap = {}
+        # for n in nums:
+        #     hashmap[n] = hashmap.get(n, 0) + 1
+        # for k, v in hashmap.items():
+        #     if v == 1:
+        #         return k
+
+        # Binary Search O(logn), O(1)
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            m = (l+r) // 2
+            if m % 2 == 1:
+                m -= 1
+            if nums[m] == nums[m+1]:
+                l = m + 2
+            else:
+                r = m
+        return nums[l]
