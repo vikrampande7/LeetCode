8b335343e6f48d36fbbc1089ffb060a8e47cc318
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-11 19:54:07 -0400

    Time: 0 ms (100%), Space: 17.8 MB (42.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        def d(dividend, divisor):
+            while dividend % divisor == 0:
+                dividend //= divisor
+            return dividend
+        for factor in [2, 3, 5]:
+            n = d(n, factor)
+        return n == 1  
