aebf28896b98633dfe89ed9c79618991463de039
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-12 16:45:50 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,29 +3,23 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         time = 0
         fresh = 0
-        Q = deque()
-
+        q = deque()
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
-                    Q.append((r, c))
+                    q.append((r, c))
 
-        DIR = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while Q and fresh > 0:
-            for o in range(len(Q)):
-                r,c = Q.popleft()
-
-                for dr, dc in DIR:
-                    row = r + dr
-                    col = c + dc
-
-                    if (row in range(ROWS) and col in range(COLS) and grid[row][col] == 1):
-                        grid[row][col] = 2
-                        Q.append((row, col))
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        while q and fresh > 0:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                for dr, dc in dirs:
+                    R, C = row+dr, col+dc        
+                    if R in range(ROWS) amd C in range(COLS) and grid[R][C] == 1:
+                        grid[R][C] = 2
                         fresh -= 1
-
-            time += 1
-
+            time += 1 
+    
         return -1 if fresh > 0 else time
