be1edb03c0a1e89bc103300cf28926532416e656
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-10-19 20:45:46 -0400

    Time: 11 ms (71.92%), Space: 18 MB (33.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # Space: O(N) and Time O(NlogN)
-        # s_sorted = ''.join(sorted(s))
-        # t_sorted = ''.join(sorted(t))
-        # return s_sorted == t_sorted
+        one = {}
+        for c in s:
+            one[c] = one.get(c, 0) + 1
+        
+        two = {}
+        for c in t:
+            two[c] = two.get(c, 0) + 1
 
-        # Space: O(1) Time: O(N+M)
-        if len(s) != len(t):
-            return False
-
-        hash_s = {}
-        hash_t = {}
-
-        for i in range(len(s)):
-            hash_s[s[i]] = 1 + hash_s.get(s[i], 0)
-            hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
-
-        return hash_s == hash_t
-
-
+        return one == two
         
