6ea6a1c61e6a5c4f33028f89cf16854b9e0de180
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-11-27 12:03:05 -0500

    Time: 3 ms (65.73%), Space: 20.1 MB (7.72%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashSet = {}
-        for num in nums:
-            hashSet[num] = hashSet.get(num, 0) + 1
-        
-        for k, v in hashSet.items():
-            if v == 1:
-                return k
-        
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                hashset.remove(n)
+            else:
+                hashset.add(n)
+        return hashset.pop()
