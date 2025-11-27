cc48855f90a95a3c09dd8c2eab36a591ea4ff1d3
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-11-27 12:10:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,9 +1,6 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num1=list(set(nums1))
-        num2=list(set(nums2))
-        common = list(set(num1) & set(num2))
-        if common:
-             return common
-        return []
-        
+        hashset_1 = set(nums1)
+        hashset_2 = set(nums2)
+
+        return (list(hashset_1) & list(hashset_2))
