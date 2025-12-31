60a8cdd4812479361bac20bac4fb357b14f79f3a
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2025-12-31 18:43:18 -0500

    Time: 9 ms (18.31%), Space: 18.7 MB (90.34%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,8 +1,8 @@
 class UnionFind:
     def __init__(self, size):
         self.parent = list(range(size))
-        self.rank = [0 for _ in range(size)]
-    
+        self.rank = [0 for i in range(size)]
+
     def find(self, x):
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
@@ -14,24 +14,19 @@ class UnionFind:
         if xset == yset:
             return
         if self.rank[xset] > self.rank[yset]:
-            self.parent[yset] = self.parent[xset]
+            self.parent[yset] = xset
         elif self.rank[xset] < self.rank[yset]:
-            self.parent[xset] = self.parent[yset]
+            self.parent[xset] = yset
         else:
-            self.parent[xset] = self.parent[yset]
-            self.rank[yset] += 1
+            self.parent[xset] = yset
+            self.rank[xset] += 1
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         uf = UnionFind(n)
-
-        # Union All
         for A, B in edges:
             uf.union(A, B)
-
         noc = set()
-        # Count the Distinct Parents
         for i in range(n):
             noc.add(uf.find(i))
-
         return len(noc)
