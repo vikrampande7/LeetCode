5ffe35ef849cf22bc9025476ad37ed1899da86c8
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-01-07 17:13:06 -0500

    Time: 8 ms (5.06%), Space: 18.1 MB (35.72%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,25 +1,15 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        adj = {src: [] for src, dst in tickets}
-        tickets.sort()
-        for src, dst in tickets:
+        adj = defaultdict(list)
+        for src, dst in sorted(tickets)[::-1]:
             adj[src].append(dst)
 
-        res = ["JFK"]
+        res = []
         def dfs(src):
-            if len(res) == len(tickets) + 1:
-                return True
-            if src not in adj:
-                return False
-
-            temp = list(adj[src])
-            for i, v in enumerate(temp):
-                adj[src].pop(i)
-                res.append(v)
-                if dfs(v): return True
-                adj[src].insert(i, v)
-                res.pop()
-            return False
+            while adj[src]:
+                dst = adj[src].pop()
+                dfs(dst)
+            res.append(src)
             
-        dfs("JFK")
-        return res
+        dfs('JFK')
+        return res[::-1]
