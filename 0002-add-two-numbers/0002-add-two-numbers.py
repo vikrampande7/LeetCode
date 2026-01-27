819fba472ad4e5394873c7956d76b709eb78db91
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-01-27 15:38:31 -0500

    Time: 3 ms (70.07%), Space: 19.5 MB (16.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,17 +5,25 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        prehead = ListNode(0)
-        curr = prehead
+        sentinel = ListNode(0)
+        prev = sentinel
         carry = 0
         while l1 or l2 or carry:
+            # Get values from each linked list
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-            valSum  = val1 + val2 + carry
-            digit = valSum % 10
-            carry = valSum // 10
-            curr.next = ListNode(digit)
-            curr = curr.next
+
+            # Find the addition, digit and carry if existst
+            addition = val1 + val2 + carry
+            digit = addition % 10
+            carry = addition // 10
+
+            # Add the new digit to the sentinel Node and update the sentinel pointer
+            prev.next = ListNode(digit)
+            prev = prev.next
+
+            # Update Lists and got to next if exist
             if l1: l1 = l1.next
             if l2: l2 = l2.next
-        return prehead.next
+
+        return sentinel.next
