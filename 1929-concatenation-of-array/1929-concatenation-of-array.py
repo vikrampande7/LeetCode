7783663d5a1a2cd3506057fd9551e188ed1a458a
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-08 14:57:38 -0400

    Time: 0 ms (100%), Space: 12.6 MB (49.21%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -4,5 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        return nums + nums
-        
+        # return nums + nums
+        out = []
+        for _ in range(2):
+            for n in nums:
+                out.append(n)
+        return out
