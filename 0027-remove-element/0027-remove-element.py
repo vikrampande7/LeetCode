b53a7a73aa82f65dcfae3a00c965e9b0c5768c77
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-11 11:28:22 -0400

    Time: 0 ms (100%), Space: 12.5 MB (6.27%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,8 +1,18 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != val:
-                nums[i] = nums[j]
-                i += 1
-        return i
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+#         Write Pointer (k): Tracks where the next "valid" element should be placed.
+# Scan Pointer (i): Iterates through the entire array.
+# Algorithm: As you scan, if nums[i] is not equal to val, copy nums[i] to nums[k] and increment k.
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+        
