72d52edf7d0c2ba75e7e6afd5c25015889bab4a7
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-11 10:21:58 -0400

    Time: 27 ms (33.25%), Space: 17.9 MB (22.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,15 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for word in strs:
-            freqCounter = [0] * 26
-            for c in word:
-                freqCounter[ord(c) - ord('a')] += 1
-            res[tuple(freqCounter)].append(word)
-        return list(res.values())
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        d = defaultdict(list)
+        out = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                idx = (ord(c) - ord('a'))
+                count[idx] += 1
+            d[tuple(count)].append(s)
+        return d.values()
