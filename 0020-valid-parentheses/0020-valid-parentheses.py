b45e4c47d75b0d20efac160648a32e8571f9fa06
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-13 18:55:47 -0400

    Time: 4 ms (42.44%), Space: 12.5 MB (45.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,25 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        if len(s) == 1:
-            return False
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         stack = []
+        closing = 0
         for c in s:
-            if c in "({[":
+            if c in "([{":
                 stack.append(c)
             else:
-                if stack:
-                    top = stack[-1]
-                    if c == ")" and top == "(" or c == "}" and top == "{" or c == "]" and top == "[":
-                        stack.pop()
-                    else:
-                        return False
-                else:
-                    return False
-        return False if stack else True
-        
+                closing += 1
+            if stack:
+                top = stack[-1]
+                if top == "(" and c == ')':
+                    stack.pop()
+                    closing -= 1
+                if top == "[" and c == ']':
+                    stack.pop()
+                    closing -= 1
+                if top == "{" and c == '}':
+                    stack.pop()
+                    closing -= 1
+        return True if (len(stack) == 0 and closing == 0) else False
