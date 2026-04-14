de3b6ead6a1e0de727cd1145643cd027fc931604
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-13 22:05:26 -0400

    Time: 3 ms (54.8%), Space: 20 MB (18.98%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,15 +1,18 @@
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
+class Solution(object):
+    def reverseString(self, s):
         """
-        Do not return anything, modify s in-place instead.
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
         """
-        # def recursion(s, l, r):
-        #     if l < r:
-        #         s[l], s[r] = s[r], s[l]
-        #         recursion(s, l+1, r-1)
-        # recursion(s, 0, len(s)-1)
-        def rec(s, l, r):
-            if l < r:
-                s[l], s[r] = s[r], s[l]
-                rec(s, l+1, r-1)
-        return rec(s, 0, len(s) - 1)
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            tmp = s[l]
+            s[l] = s[r]
+            s[r] = tmp
+            l += 1
+            r -= 1
+
+        return s
+        
