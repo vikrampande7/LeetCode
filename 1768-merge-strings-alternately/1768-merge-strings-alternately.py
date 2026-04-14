3f3cd9c0ae718353daff82b9a157857a3f6f64a3
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-13 22:14:40 -0400

    Time: 19 ms (34.78%), Space: 12.4 MB (31.2%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,18 +1,17 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = ""
-        if len(word1) > len(word2):
-            for i in range(len(word2)):
-                res += word1[i]
-                res += word2[i]
-            res += word1[len(word2):]
-        elif len(word2) > len(word1):
-            for i in range(len(word1)):
-                res += word1[i]
-                res += word2[i]
-            res += word2[len(word1):]
-        else:
-            for i in range(len(word1)):
-                res += word1[i]
-                res += word2[i]
-        return res
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        minLen = min(len(word1), len(word2))
+        out = ""
+        for i in range(minLen):
+            out += word1[i]
+            out += word2[i]
+        if len(word1) > minLen:
+            out += word1[minLen:]
+        if len(word2) > minLen:
+            out += word2[minLen:]
+        return out
