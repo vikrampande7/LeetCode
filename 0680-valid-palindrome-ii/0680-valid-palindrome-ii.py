03c8a3e2c075c4f4061057870fd1168b4ce1c124
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-23 12:33:46 -0400

    Time: 82 ms (54.78%), Space: 12.5 MB (74.13%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -16,4 +16,6 @@ class Solution(object):
         while l < r:
             if s[l] != s[r]:
                 return (checkPalindrome(s, l, r - 1) or checkPalindrome(s, l + 1, r))
+            l += 1
+            r -= 1
         return True
