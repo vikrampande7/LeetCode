9a4028c070e3ba311db80b110ee65f64a6f70552
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-04-28 19:03:13 -0400

    Time: 33 ms (46.07%), Space: 20.3 MB (49.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,14 +1,15 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # easy way: product of all elements in array and divide by each element
-        # edge case - division by zero
-        # Best approach -> prefix, postfix -> Neetcode
-        res = [1] * len(nums)
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        prefixSum = [1] * len(nums)
         prefix, postfix = 1, 1
         for i in range(len(nums)):
-            res[i] = prefix
+            prefixSum[i] = prefix
             prefix *= nums[i]
-        for i in range(len(nums)-1,-1,-1):
-            res[i] *= postfix
+        for i in range(len(nums)-1, -1, -1):
+            prefixSum[i] *= postfix
             postfix *= nums[i]
-        return res
+        return prefixSum
