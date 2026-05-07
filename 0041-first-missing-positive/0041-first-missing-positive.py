3c92136aa37d9ae251cb6454fa793d3651709c38
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-05-07 14:10:50 -0400

    Time: 38 ms (84.43%), Space: 21 MB (18.75%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,12 +1,16 @@
-class Solution:
-    def firstMissingPositive(self, nums: List[int]) -> int:
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         n = len(nums)
-        seen = [False for i in range(0, n+1)]
+        seen = [False] * (n+1)
         for num in nums:
             if 0 < num <= n:
                 seen[num] = True
-        minVal = float("inf")
         for i in range(1, n+1):
             if not seen[i]:
                 return i
         return n + 1
+        
