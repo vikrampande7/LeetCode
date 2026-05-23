dd0ae9dcc311bc57a1ba8e0f847684aab1a70c0d
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-05-23 11:08:33 -0400

    Time: 692 ms (8.93%), Space: 19 MB (6.26%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,42 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        ### DFS RECURSIVE
-        # if not root:
-        #     return 0
-        # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-
-        ### DFS with Stack - Iterative - PreOrder
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
         if not root:
             return 0
-        stack = [[root, 1]]
-        res = 1
-        while stack:
-            currNode, depth = stack.pop()
-            res = max(res, depth)
-            if currNode.left:
-                stack.append([currNode.left, depth+1])
-            if currNode.right:
-                stack.append([currNode.right, depth+1])
-        return res
-
-        ### BFS with Queue - Iterative
-        # if not root:
-        #     return 0
-        # q = deque([root])
-        # level = 0
-        # while q:
-        #     for i in range(len(q)):
-        #         currNode = q.popleft()
-        #         if currNode.left:
-        #             q.append(currNode.left)
-        #         if currNode.right:
-        #             q.append(currNode.right)
-        #     level += 1
-        # return level
-        
+        queue = deque([root])
+        print(queue)
+        level = 0
+        while queue:
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level += 1
+        return level
