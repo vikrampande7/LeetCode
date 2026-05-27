df01e2cacb33fdf90a6eebd2f88e186b3db30dda
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-05-27 15:34:41 -0400

    Time: 29 ms (25.54%), Space: 19.1 MB (5.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        ans = defaultdict(list)
-        for s in strs:
-            anagrams = [0] * 26
-            for c in s:
+        ans = defaultdict(list) # {key (26 chars): [words]}
+        for word in strs:
+            anagrams = 26 * [0]
+            for c in word:
                 idx = ord(c) - ord('a')
                 anagrams[idx] += 1
-            ans[tuple(anagrams)].append(s)
+            ans[tuple(anagrams)].append(word)
         return [v for k, v in ans.items()]
