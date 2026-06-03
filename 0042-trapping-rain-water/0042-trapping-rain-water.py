9a2d9bb3dbc81b829bb9a890467dc08912a30ad0
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-06-03 17:32:31 -0400

    Time: 15 ms (40.72%), Space: 13.5 MB (57.33%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -20,20 +20,37 @@ class Solution(object):
         # return water
 
         # Extra Memory
+        # n = len(height)
+        # left = [0] * n
+        # right = [0] * n
+        # right_max = 0
+        # left_max = 0
+        # for i in range(n):
+        #     if height[i] >= right_max:
+        #         right_max = max(right_max, height[i])
+        #     left[i] = right_max
+        # for i in range(n-1, -1, -1):
+        #     if left_max <= height[i]:
+        #         left_max = max(left_max, height[i])
+        #     right[i] = left_max
+        # water = 0
+        # for i in range(n):
+        #     water += max(0, min(left[i], right[i]) - height[i])
+        # return water
+
         n = len(height)
-        left = [0] * n
-        right = [0] * n
-        right_max = 0
-        left_max = 0
-        for i in range(n):
-            if height[i] >= right_max:
-                right_max = max(right_max, height[i])
-            left[i] = right_max
-        for i in range(n-1, -1, -1):
-            if left_max <= height[i]:
-                left_max = max(left_max, height[i])
-            right[i] = left_max
+        l = 0
+        r = n - 1
+        left_max, right_max = 0, 0
         water = 0
-        for i in range(n):
-            water += max(0, min(left[i], right[i]) - height[i])
+        while l < r:
+            if height[l] < height[r]:
+                left_max = max(left_max, height[l])
+                water += left_max - height[l]
+                l += 1
+            else:
+                right_max = max(right_max, height[r])
+                water += right_max - height[r]
+                r -= 1
         return water
+
