19dfc1a45f1031b625a2fd7088982b01c3ed2dc7
Vikram Pande <70894123+vikrampande7@users.noreply.github.com>
2026-07-09 07:51:54 -0400

    Time: 4379 ms (7.69%), Space: 58.3 MB (15.38%) - LeetHub

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sumAndMultiply(self, s, queries):
+        MOD = 10**9 + 7
+        pow10 = [1] * 100001
+        for i in range(1, 100001):
+            pow10[i] = pow10[i - 1] * 10 % MOD
+
+        n = len(s)
+        sum = [0] * (n + 1)
+        x = [0] * (n + 1)
+        cnt = [0] * (n + 1)
+        for i, c in enumerate(s):
+            d = int(c)
+            sum[i + 1] = sum[i] + d
+            x[i + 1] = (x[i] * 10 + d) % MOD if d > 0 else x[i]
+            cnt[i + 1] = cnt[i] + (d > 0)
+
+        m = len(queries)
+        res = [0] * m
+        for i in range(m):
+            l = queries[i][0]
+            r = queries[i][1] + 1
+            length = cnt[r] - cnt[l]
+            res[i] = (x[r] - x[l] * pow10[length]) * (sum[r] - sum[l]) % MOD
+
+        return res
